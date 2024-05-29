826ef7379ac49fef377d81fb5cb3c55f9505a582
areiahna <areiahnacooks@yahoo.com>
2024-05-28 23:17:11 -0400

    Data Types & type() Function activity complete!

---
+++ data-types-functions.py
@@ -0,0 +1,11 @@
+# Given below are some variable assignments. Your task is to identify the data type of each variable and then use the type() function to print it out:
+
+variable_a = "Hello, World!" #string
+variable_b = 23 #integer
+variable_c = 3.14 #float
+variable_d = True #boolean
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
