9023dd37a8cb89c9d9b49667ac961de58390f304
areiahna <areiahnacooks@yahoo.com>
2024-05-28 22:49:07 -0400

    Dance moves activity complete!

---
+++ dance-moves.py
@@ -0,0 +1,18 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#============================================#
+
+mood = input("How are you feeling today? ").lower()
+
+# print(mood)
+
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
+
